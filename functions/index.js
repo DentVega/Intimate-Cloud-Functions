ce07f1ad8029f52390d04dbcbfe987995b7adc31
Brian Vega <bdvega7@gmail.com>
2020-08-14 05:39:35 -0400

    init project

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,9 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const userController = require('./components/users/UserController.js');
 
-admin.initializeApp();
-admin.firestore().settings({ timestampsInSnapshots: true });
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
-
-exports.creaccionUsuario = functions.auth
-  .user()
-  .onCreate(userController.userCreationController);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
